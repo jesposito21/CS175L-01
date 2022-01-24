82e3ceeb58124476f87c599436545a59b92bf6b4
jesposito21 <91150841+jesposito21@users.noreply.github.com>
2022-01-24 16:41:35 -0500

    Add files via upload

---
+++ myInfo.py
@@ -0,0 +1,11 @@
+#CS175L-01
+#Jenna Esposito
+#This is my information
+name= 'Jenna Esposito'
+address= '32 Seward Avenue, Toms River, NJ 08753'
+number= '908-910-1526'
+major= 'Computer Science'
+print('My name is ',name)
+print('My address is ',address)
+print('My telephone number is ', number)
+print('My college major is ', major)
